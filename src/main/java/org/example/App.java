50c9d70fabd82d6839a6e73dcf7616c1ecab4580
yonguk <kimyu9827@naver.com>
2023-10-26 15:00:17 +0900

    remove comment

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,8 @@
 package org.example;
 
 import org.example.controller.CmdController;
-import org.example.controller.Request;
-import org.example.model.Command;
 import org.example.repository.SayRepostiory;
 import org.example.service.SayService;
-
-import java.util.Scanner;
 
 public class App {
     public void run(){
